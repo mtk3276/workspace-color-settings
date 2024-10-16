7798d0c4b10a9974ad7ed65cb85b71667da1722a
Muhsina Kamal <muhsina_k@hotmail.co.uk>
2024-10-16 13:06:25 +0100

    feat: router added to root render

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import routes from '@/router.jsx';
 import './index.css'
+
+const router = createBrowserRouter(routes);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
